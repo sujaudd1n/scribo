f8d232feb1abfe2b33bfb96f9ba7186ee9e3898c
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-06-25 14:35:10 +0530

    Update tests

--- scribo.py
+++ scribo.py
@@ -6,8 +6,14 @@ import argparse
 
 def main():
     cl_args = parse_command_line_args()
+
     if cl_args.init:
-        create_project_structure(cl_args.init)
+        project_name = cl_args.init
+        if os.path.exists(project_name):
+            sys.exit(f"Project: {project_name} already exists!")
+        else:
+            create_project_structure(project_name)
+
 
 
 def parse_command_line_args():
@@ -18,11 +24,13 @@ def parse_command_line_args():
 def create_project_structure(project_name):
     os.mkdir(project_name)
     os.chdir(project_name)
-    os.makedirs("md/getting-started", exist_ok=True)
+    pathlib.Path("index.html").touch()
+    os.makedirs("md/getting-started")
     pathlib.Path("md/getting-started/index.md").touch()
     os.makedirs("style")
+    pathlib.Path("style/style.css").touch()
     os.makedirs("script")
-    pathlib.Path("index.html").touch()
+    pathlib.Path("script/script.js").touch()
     os.chdir('..')
 
 if __name__ == '__main__':
--- test_scribo.py
+++ test_scribo.py
@@ -5,13 +5,26 @@ import shutil
 from scribo import create_project_structure
 
 
+def list_files(dir):
+    result = []
+    for path, dirs, files in os.walk(dir):
+        for file in files:
+            result.append(os.path.join(path, file)) 
+    return result
+
+
 def test_create_project_structure():
     PROJECT_NAME = 'myproject'
     if os.path.exists(PROJECT_NAME):
         shutil.rmtree(PROJECT_NAME)
+
     create_project_structure(PROJECT_NAME)
+
+    assert len(list_files(PROJECT_NAME)) == 4
     assert os.path.exists(PROJECT_NAME)
     assert os.path.exists(os.path.join(PROJECT_NAME, "md/getting-started"))
     assert os.path.exists(os.path.join(PROJECT_NAME, "script"))
     assert os.path.exists(os.path.join(PROJECT_NAME, "style"))
+    assert os.path.exists(os.path.join(PROJECT_NAME, "style/style.css"))
+    assert os.path.exists(os.path.join(PROJECT_NAME, "script/script.js"))
     assert os.path.exists(os.path.join(PROJECT_NAME, "index.html"))
