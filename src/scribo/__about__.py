68fda04bd4b7768918ae5fbe4042711123bba500
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2025-01-23 18:35:56 +0530

    Update exclude of build

--- src/scribo/__about__.py
+++ src/scribo/__about__.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: 2024-present Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
-#
-# SPDX-License-Identifier: MIT
 __version__ = "1.0.0rc0"
 __author__ = "Md Sujauddin Sekh"
 __license__ = "MIT"
